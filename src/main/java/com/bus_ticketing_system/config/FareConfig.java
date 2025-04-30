1eaa5ef0e80657cb42db05456d276425a66e941b
motaamneh <mtaamneh292@gmail.com>
2025-04-30 21:02:53 +0300

    FareConfig logic and webxml props

--- src/main/java/com/bus_ticketing_system/config/FareConfig.java
+++ src/main/java/com/bus_ticketing_system/config/FareConfig.java
@@ -1,5 +1,7 @@
 package com.bus_ticketing_system.config;
 
+import jakarta.servlet.ServletContext;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,7 +15,40 @@ public class FareConfig {
     private FareConfig() {
         fareMultipliers = new HashMap<String , Double>();
         discountPercentages = new HashMap<String , Double>();
-        
+        refreshRules();
+
+    }
+
+    public static synchronized FareConfig getInstance() {
+        if(instance == null) {
+            instance = new FareConfig();
+        }
+        return instance;
+    }
+
+    public void initialize(ServletContext servletContext) {
+        String studentDiscountStr = servletContext.getInitParameter("student.discount");
+        String seniorDiscountStr = servletContext.getInitParameter("senior.discount");
+        String eventDiscountStr = servletContext.getInitParameter("event.discount");
+
+        if(studentDiscountStr != null){
+            discountPercentages.put(studentDiscountStr, Double.parseDouble(studentDiscountStr));
+        }
+        if(seniorDiscountStr != null){
+            discountPercentages.put(seniorDiscountStr, Double.parseDouble(seniorDiscountStr));
+        }
+        if(eventDiscountStr != null){
+            discountPercentages.put(eventDiscountStr, Double.parseDouble(eventDiscountStr));
+        }
+
+
+    }
+
+
+
+
+
+    public synchronized void refreshRules() {
 
     }
 
